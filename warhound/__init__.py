f092070f3c5f0c367bfee76b4ba70ecc0563dc3e
Joshua Morris <joshua.a.morris@gmail.com>
2018-01-02 01:22:42 -0800

    wip: prettify core processing loop

--- warhound/__init__.py
+++ warhound/__init__.py
@@ -11,9 +11,8 @@ class Telemetry(namedtuple('Telemetry', ['matchmaking', 'match', 'outcome'])):
     __slots__ = ()
 
 
-    def __new__(cls, _matchmaking, _match, _outcome):
-        return super(Telemetry, cls).__new__(cls, _matchmaking, _match,
-                                             _outcome)
+    def __new__(cls, matchmaking, match, outcome):
+        return super(Telemetry, cls).__new__(cls, matchmaking, match, outcome)
 
 
 def attempt(obj_event, dict_processor_by_event_type, dest,state):
@@ -33,26 +32,26 @@ def test_round_finished(event):
 def process(obj):
     list_round_finished_event = list(filter(test_round_finished, obj))
 
-    _matchmaking = mk_empty_matchmaking()
-    _match       = mk_empty_match(len(list_round_finished_event))
-    _outcome     = mk_empty_outcome()
+    matchmaking = mk_empty_matchmaking()
+    match       = mk_empty_match(len(list_round_finished_event))
+    outcome     = mk_empty_outcome()
 
     state = { 'round': 0, 'dict_team_id_by_player_id': {},
               'dict_side_by_player_id': {} }
 
-    for _evt in sorted(obj, key=lambda e: e['cursor']):
-        attempt(_evt, mm.PROCESSOR_BY_EVENT_TYPE, _matchmaking, state)
-        attempt(_evt,  m.PROCESSOR_BY_EVENT_TYPE,       _match, state)
+    for event in sorted(obj, key=lambda e: e['cursor']):
+        attempt(event, mm.PROCESSOR_BY_EVENT_TYPE, matchmaking, state)
+        attempt(event,  m.PROCESSOR_BY_EVENT_TYPE,       match, state)
 
-        if _evt['type'] in r.PROCESSOR_BY_EVENT_TYPE:
-            if 'round' in _evt['dataObject']:
-                _round = _match.list_round[_evt['dataObject']['round']]
+        if event['type'] in r.PROCESSOR_BY_EVENT_TYPE:
+            if 'round' in event['dataObject']:
+                _round = match.list_round[event['dataObject']['round']]
             else:
-                _round = _match.list_round[state['round']]
+                _round = match.list_round[state['round']]
 
-            attempt(_evt, r.PROCESSOR_BY_EVENT_TYPE, _round, state)
+            attempt(event, r.PROCESSOR_BY_EVENT_TYPE, _round, state)
 
-        attempt(_evt, o.PROCESSOR_BY_EVENT_TYPE, _outcome, state)
+        attempt(event, o.PROCESSOR_BY_EVENT_TYPE, outcome, state)
 
-    return Telemetry(_matchmaking, _match, _outcome)
+    return Telemetry(matchmaking, match, outcome)
 
