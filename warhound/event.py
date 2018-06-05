222255d121b85a8d0b7e847c5c3d6fdd9f4096d6
Joshua Morris <joshua.a.morris@gmail.com>
2018-06-05 02:45:32 -0700

    further work refining events

--- warhound/event.py
+++ warhound/event.py
@@ -1,7 +1,8 @@
 from collections import namedtuple
 import enum
 
-# The telemetry file format is simple: it is a json file-formatted list of
+
+# The telemetry file format is simple: it is a source_json file-formatted list of
 # events. Here is what an example event from this list looks like:
 #
 # {
@@ -22,7 +23,7 @@ import enum
 #
 # Events ordered by 'cursor' value are chronological. They are merely a stream--
 # we have to deduce certain boundaries like "when round 2 started" by
-# inference. (In this case, we must look for the first event with type
+# inference. (In this case, we must look for the first event with kind
 # "Structures.RoundFinished" and pay attention to what follows until the next).
 #
 # This module pigeonholes events into one of the following categories:
@@ -49,15 +50,13 @@ import enum
 # information about one spell for one hero.
 
 
-EVT_ONLY_CLASS = 0xf0000000
-EVT_ONLY_TYPE = 0x0fffffff
-
-EVT_MASK_MATCH = 0x80000000
-EVT_MASK_ROUND = 0x40000000
-EVT_MASK_SPELLSUMMARY = 0x20000000
+class Mask(enum.IntEnum):
+    MATCH = 0x80000000
+    ROUND = 0x40000000
+    SPELLSUMMARY = 0x20000000
 
 
-class EventType(enum.IntEnum):
+class Kind(enum.IntEnum):
     # Match events, which pertain to the entire game:
     MATCH_UNKNOWN = 0x8fffffff 
     MATCH_BATTLERITEPICK = 0x80000001
@@ -94,51 +93,64 @@ class EventType(enum.IntEnum):
 
 
 DICT_MATCH_EVENT_BY_DESCRIPTOR = {
-    'Structures.BattleritePickEvent': EventType.MATCH_BATTLERITEPICK,
-    'Structures.MatchFinishedEvent': EventType.MATCH_FINISHED,
-    'com.stunlock.service.matchmaking.avro.QueueEvent': EventType.MATCH_QUEUE,
-    'Structures.MatchReservedUser': EventType.MATCH_RESERVEDUSER,
-    'Structures.ServerShutdown': EventType.MATCH_SERVERSHUTDOWN,
-    'Structures.MatchStart': EventType.MATCH_START,
-    'com.stunlock.battlerite.team.TeamUpdateEvent': EventType.MATCH_TEAMUPDATE,
+    'Structures.BattleritePickEvent': Kind.MATCH_BATTLERITEPICK,
+    'Structures.MatchFinishedEvent': Kind.MATCH_FINISHED,
+    'com.stunlock.service.matchmaking.avro.QueueEvent': Kind.MATCH_QUEUE,
+    'Structures.MatchReservedUser': Kind.MATCH_RESERVEDUSER,
+    'Structures.ServerShutdown': Kind.MATCH_SERVERSHUTDOWN,
+    'Structures.MatchStart': Kind.MATCH_START,
+    'com.stunlock.battlerite.team.TeamUpdateEvent': Kind.MATCH_TEAMUPDATE,
 }
 
 
 DICT_ROUND_EVENT_BY_DESCRIPTOR = {
     ('Structures.RoundEvent', 'ALLY_DEATH_ENERGY_PICKUP'):
-        EventType.ROUND_ALLYDEATHENERGYPICKUP,
-    ('Structures.DeathEvent', None): EventType.ROUND_DEATH,
+        Kind.ROUND_ALLYDEATHENERGYPICKUP,
+    ('Structures.DeathEvent', None): Kind.ROUND_DEATH,
     ('Structures.RoundEvent', 'ENERGY_ABILITY_USED'):
-        EventType.ROUND_ENERGYABILITYUSED,
+        Kind.ROUND_ENERGYABILITYUSED,
     ('Structures.RoundEvent', 'ENERGY_SHARD_PICKUP'):
-        EventType.ROUND_ENERGYSHARDPICKUP,
-    ('Structures.RoundFinishedEvent', None): EventType.ROUND_FINISHED,
+        Kind.ROUND_ENERGYSHARDPICKUP,
+    ('Structures.RoundFinishedEvent', None): Kind.ROUND_FINISHED,
     ('Structures.RoundEvent', 'HEALTH_SHARD_PICKUP'):
-        EventType.ROUND_HEALTHSHARDPICKUP,
-    ('Structures.RoundEvent', 'MOUNT_DURATION'): EventType.ROUND_MOUNTDURATION,
-    ('Structures.RoundEvent', 'RUNE_LASTHIT'): EventType.ROUND_RUNELASTHIT,
-    ('Structures.RoundEvent', 'ULTIMATE_USED'): EventType.ROUND_ULTIMATEUSED,
+        Kind.ROUND_HEALTHSHARDPICKUP,
+    ('Structures.RoundEvent', 'MOUNT_DURATION'): Kind.ROUND_MOUNTDURATION,
+    ('Structures.RoundEvent', 'RUNE_LASTHIT'): Kind.ROUND_RUNELASTHIT,
+    ('Structures.RoundEvent', 'ULTIMATE_USED'): Kind.ROUND_ULTIMATEUSED,
 }
 
 
 DICT_SPELLSUMMARY_EVENT_BY_DESCRIPTOR = {
-    'CONTROL_DONE': EventType.SPELLSUMMARY_CONTROLDONE,
-    'CONTROL_RECEIVED': EventType.SPELLSUMMARY_CONTROLRECEIVED,
-    'DAMAGE_DONE': EventType.SPELLSUMMARY_DAMAGEDONE,
-    'DAMAGE_RECEIVED': EventType.SPELLSUMMARY_DAMAGERECEIVED,
-    'ENERGY_RECEIVED': EventType.SPELLSUMMARY_ENERGYRECEIVED,
-    'ENERGY_USED': EventType.SPELLSUMMARY_ENERGYUSED,
-    'HEALING_DONE': EventType.SPELLSUMMARY_HEALINGDONE,
-    'HEALING_RECEIVED': EventType.SPELLSUMMARY_HEALINGRECEIVED,
-    'USES': EventType.SPELLSUMMARY_USES,
+    'CONTROL_DONE': Kind.SPELLSUMMARY_CONTROLDONE,
+    'CONTROL_RECEIVED': Kind.SPELLSUMMARY_CONTROLRECEIVED,
+    'DAMAGE_DONE': Kind.SPELLSUMMARY_DAMAGEDONE,
+    'DAMAGE_RECEIVED': Kind.SPELLSUMMARY_DAMAGERECEIVED,
+    'ENERGY_RECEIVED': Kind.SPELLSUMMARY_ENERGYRECEIVED,
+    'ENERGY_USED': Kind.SPELLSUMMARY_ENERGYUSED,
+    'HEALING_DONE': Kind.SPELLSUMMARY_HEALINGDONE,
+    'HEALING_RECEIVED': Kind.SPELLSUMMARY_HEALINGRECEIVED,
+    'USES': Kind.SPELLSUMMARY_USES,
 }
 
 
-def infer_maybe_spellsummary_event_type(evt_json):
+class Event(namedtuple('Event', ['kind', 'source_json'])):
+    def __new__(cls, kind, source_json):
+        return super().__new__(cls, kind, source_json)
+
+
+def mk(*args, **kwargs):
+    return Event(*args, **kwargs)
+
+
+def from_source_json(source_json):
+    return mk(infer_event_kind(source_json), source_json)
+
+
+def infer_maybe_spellsummary_event_kind(source_json):
     """Return either spell summary event or None."""
 
-    if evt_json['type'] == 'Structures.UserRoundSpell':
-        descriptor = evt_json['dataObject']['scoreType']
+    if source_json['type'] == 'Structures.UserRoundSpell':
+        descriptor = source_json['dataObject']['scoreType']
 
         try:
             return DICT_SPELLSUMMARY_EVENT_BY_DESCRIPTOR[descriptor]
@@ -148,65 +160,52 @@ def infer_maybe_spellsummary_event_type(evt_json):
         return None
 
 
-def infer_maybe_round_event_type(evt_json):
+def infer_maybe_round_event_kind(source_json):
     """Return either round event or None."""
 
-    evt_type = evt_json['type']
+    kind = source_json['type']
 
-    if evt_type in ('Structures.DeathEvent', 'Structures.RoundFinishedEvent'):
-        evt_subtype = None
+    if kind in ('Structures.DeathEvent', 'Structures.RoundFinishedEvent'):
+        subkind = None
     else:
         try:
-            evt_subtype = evt_json['dataObject']['type']
+            subkind = source_json['dataObject']['type']
         except KeyError:
             return None
 
     try:
-        return DICT_ROUND_EVENT_BY_DESCRIPTOR[(evt_type, evt_subtype)]
+        return DICT_ROUND_EVENT_BY_DESCRIPTOR[(kind, subkind)]
     except KeyError:
-        if evt_type == 'Structures.RoundEvent':
+        if kind == 'Structures.RoundEvent':
             return Event.ROUND_UNKNOWN
         else:
             return None
 
 
-def infer_maybe_match_event_type(evt_json):
+def infer_maybe_match_event_kind(source_json):
     """Return either Match event or None."""
 
     try:
-        return DICT_MATCH_EVENT_BY_DESCRIPTOR[evt_json['type']]
+        return DICT_MATCH_EVENT_BY_DESCRIPTOR[source_json['type']]
     except KeyError:
         return Event.MATCH_UNKNOWN
 
 
-def infer_event_type(evt_json):
-    """Given raw input from telemetry, return discrete type or None."""
+def infer_event_kind(source_json):
+    """Given raw input from telemetry, return discrete kind or None."""
     
     # infer_maybe_match_event defaults to "unknown match event."
     # In other words, at least one heuristic will return something.
     list_heuristic = [
-        lambda evt_json: infer_maybe_spellsummary_event_type(evt_json),
-        lambda evt_json: infer_maybe_round_event_type(evt_json),
-        lambda evt_json: infer_maybe_match_event_type(evt_json)
+        lambda source_json: infer_maybe_spellsummary_event_kind(source_json),
+        lambda source_json: infer_maybe_round_event_kind(source_json),
+        lambda source_json: infer_maybe_match_event_kind(source_json)
     ]
 
     for heuristic in list_heuristic:
-        result = heuristic(evt_json)
+        result = heuristic(source_json)
         if result:
             return result
     else:
         raise RuntimeError('insanity') # See comments above.
-
-
-class Event(namedtuple('Event', ['evt_type', 'evt_json'])):
-    def __new__(cls, evt_type, evt_json):
-        return super().__new__(cls, evt_type, evt_json)
-
-
-def mk(*args, **kwargs):
-    return Event(*args, **kwargs)
-
-
-def from_json(evt_json):
-    return mk(infer_event_type(evt_json), evt_json)
 
